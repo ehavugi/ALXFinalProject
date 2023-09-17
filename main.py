752c592887617a1299117a3338e13d343ea7346a
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-09-16 18:19:35 -0700

    added model versioning for a simple regression problem

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ JSON based data saving in sqlite db and retrieval of sqlite data.
     Sept 16, 2023.
     Work towards ALX profolio project
 
-Model serving with model id saved with client id
+Model serving with model id saved with client id.
+
+    + features:
+        + training a simple model (regression model given 
+            + two inputs [2 columns]  input: x : [1,2], y: [1,2]
+            + and output [1 column])  output : "VL": [1, 1]
+        + model versioninng with a random key generated each time a model train is posted.
+        + model id is used for using the model.
+            + model(Structure, input-> put contrained) is currently fixed
 """
 import flask
 import time
@@ -11,7 +19,11 @@ import time
 app = flask.Flask(__name__)
 
 import sqlite3
+import json
+
 from flask import jsonify
+import numpy as np
+from sklearn.linear_model import LinearRegression
 conn = sqlite3.connect('mydatabase.sqlite')
 
 c = conn.cursor()
@@ -19,10 +31,10 @@ c = conn.cursor()
 # Create a table
 # c.execute("DROP TABLE IF EXISTS mytable2")
 c.execute('CREATE TABLE IF NOT EXISTS mytable2 (id INTEGER PRIMARY KEY, REF TEXT, data BLOB)')
-binary_data = b'38434'
-ref = 1
-# Insert binary data into the column
-c.execute('INSERT INTO mytable2 (REF, data) VALUES (?,?)', (1, binary_data,))
+# binary_data = b'38434'
+# ref = 1
+# # Insert binary data into the column
+# c.execute('INSERT INTO mytable2 (REF, data) VALUES (?,?)', (1, binary_data,))
 
 # Commit the changes to the database
 conn.commit()
@@ -33,6 +45,10 @@ conn.close()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    """
+    This project entry point. Would have links to other end points or call them.
+    to be implemented further
+    """
     if flask.request.method == "POST":
         data = flask.request.get_json()
         # do something with the data
@@ -42,6 +58,10 @@ def index():
 
 @app.route("/train", methods=["GET", "POST"])
 def trainingModel():
+    """
+    Post -> model training data, for now there is one model. List of models and input, 
+    output structures can be list in advanced implementation
+    """
     if flask.request.method == "POST":
         current_time = time.time()
         conn = sqlite3.connect('mydatabase.sqlite')
@@ -50,10 +70,25 @@ def trainingModel():
 
         data = flask.request.get_json()
         # do something with the data
+        X = data['input']['x']
+        Y = data['input']['y']
+
+        INPUT = np.array([X, Y]).T
+        Z = data['output']['VL']
+        output = np.array(Z).reshape(-1,1)
+        newData = {}
+        data = newData
+        reg = LinearRegression().fit(INPUT, output)
+        data['input_shape'] = INPUT.shape
+        data['output_shape'] = output.shape
+        data['intercept'] = reg.intercept_.tolist()
+        data['score'] = str(reg.score(INPUT,output))
+        data['coeff'] =reg.coef_.tolist()
+        
         data['time'] = current_time
         data['modelReference'] =  hash(str(data))
         data['trained'] = True
-        c.execute('INSERT INTO mytable2 (REF, data) VALUES (?,?)', (     data['modelReference'],str(data),))
+        c.execute('INSERT INTO mytable2 (REF, data) VALUES (?,?)', (     data['modelReference'],json.dumps(data),))
 
         # Commit the changes to the database
         conn.commit()
@@ -83,20 +118,25 @@ def trainingModel():
            
                 # return str(results)
                 # print(data,'34')
+                # intercept = data[0].get('intercept', 0)
+                results = json.loads(str(results[0][0].replace("'",'"')))
+                results
+                coeff =  np.array(results['coeff'])  
+                intercept_ =np.array(results['intercept']).T
+                X = data['input']['x']
+                Y = data['input']['y']
+
+                INPUT = np.array([X, Y], dtype='f').T
+                output =  np.dot(INPUT,coeff.T) + intercept_
+
+                return jsonify(output.tolist())
+
+                print(jsonify(results[0][0]))#W#['intercept'])
                 jsonify(data)
                 # return data
                 return jsonify(results[0])
 
-        #     # print the results
-        #     # for row in results:
-        #     #   print(row)
 
-
-        #     # Commit the changes to the database
-        #     conn.commit()
-
-        #     # Close the connection
-        #     return results
         else:
             data['Modeled'] = "NOT CHECKED"
             data['results'] = [str(results), modelReference]
--- test.py
+++ test.py
@@ -1,12 +1,45 @@
 import requests
 
 # Create a request object
-# response = requests.post('http://127.0.0.1:5000/train', json={'input': {'x':[123,23],"y":[2,23],},"output":{"VL":[2,2]},})
-response = requests.get('http://127.0.0.1:5000/train', json={'input': {"modelReference":"-2803657691561295276"},})
+# response = requests.post('http://127.0.0.1:5000/train', json={'input': {'x':[6,5],"y":[2,5],},"output":{"VL":[12,55]},})
+# response = requests.get('http://127.0.0.1:5000/train', json={'input': {"modelReference":"-2803657691561295276"},})
+response = requests.get('http://127.0.0.1:5000/train', json={'input': {'x':[13,35,23],"y":[6,23,35],"modelReference":"1157746954101703716"}})
 
+print(response.request)
 # Check the response status code
 if response.status_code == 201:
   pass
 else:
   print(response.text)
   # The request failed
+
+
+# {
+#   "coeff": [
+#     [
+#       6.879999999999999,
+#       5.1599999999999975
+#     ]
+#   ],
+#   "input_shape": [
+#     2,
+#     2
+#   ],
+#   "intercept": [
+#     -5.199999999999989
+#   ],
+#   "modelReference": -4577614554448436428,
+#   "output_shape": [
+#     2,
+#     1
+#   ],
+#   "score": "1.0",
+#   "time": 1694907978.396081,
+#   "trained": true
+# }
+
+# [Finished in 3.0s]
+
+
+# curl -d '{"input":{"x":[13,35,23],"y":[6,23,35],"modelReference":"1157746954101703716"}}' -H "Content-Type: application/json" -X GET http://localhost:5000/train
+# curl --json '{"input":{"x":[6,5],"y":[2,5]},"output":{"VL":[12,55]}}' -X POST http://localhost:5000/train
